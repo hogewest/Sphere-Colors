9a25602242b885bcf656354555d9e6c3abd47982
hogewest <hogewest@gmail.com>
2013-06-27 02:47:26 +0900

    change app.rb

--- app.rb
+++ app.rb
@@ -3,15 +3,15 @@ require 'sinatra'
 require 'haml'
 require 'json'
 
+configure do
+  set :haml, ugly: false, attr_wrapper: '"', escape_html: true
+end
+
 configure :production do
   require "newrelic_rpm"
 end
 
-before do
-  set :haml, :format => :html5
-  @sphere = JSON.parse(File.read('./public/sphere.json'))
-end
-
 get '/' do
+  @sphere = JSON.parse(File.read('./public/sphere.json'))
   haml :index
 end
