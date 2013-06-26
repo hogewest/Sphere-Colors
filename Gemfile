99e1b942be7c59a97d56c1ff12c2cf2ecc92fee2
hogewest <hogewest@gmail.com>
2013-06-27 02:08:19 +0900

    add newrelic gem

--- Gemfile
+++ Gemfile
@@ -1,6 +1,11 @@
 source "http://rubygems.org"
+ruby "1.9.3"
 
 gem 'haml'  
 gem 'sinatra'
 gem 'sinatra-reloader'
 gem 'json'
+
+group :production do
+  gem "newrelic_rpm"
+end
--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ require 'sinatra'
 require 'haml'
 require 'json'
 
+configure :production do
+  require "newrelic_rpm"
+end
+
 before do
   set :haml, :format => :html5
   @sphere = JSON.parse(File.read('./public/sphere.json'))
